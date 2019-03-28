0648086010bfea300db784d82b2aea8222dbf534
张江浩 <zeno.jh.zhang@gmail.com>
2019-03-28 14:25:25 +0800

    1.医生视频问诊

--- src/components/Reservation/videoTest.vue
+++ src/components/Reservation/videoTest.vue
@@ -4,7 +4,7 @@
     <div ref="container" class="container">
       <div class="bigVideo">
         <video id="remoteVideo" class="video" autoplay playsinline/>
-        <p>等待医生中</p>
+        <p>等待患者中</p>
       </div>
       <div id="smallVideo" class="smallVideo">
         <video id="localVideo" class="video" muted autoplay playsinline/>
@@ -26,18 +26,31 @@ export default {
     this.$nextTick(function generate() {
       this.connectVideo()
       this.predefinedRoomId = sessionStorage.getItem('userReservationUuId')
-      this.openRoom()
+      this.openOrJoinRoom()
     })
   },
   destroyed() {
     this.connection.closeSocket()
   },
   methods: {
-    openRoom() {
-      this.connection.open(this.predefinedRoomId)
-    },
-    joinRoom() {
-      this.connection.join(this.predefinedRoomId)
+    openOrJoinRoom() {
+      this.connection.openOrJoin(this.predefinedRoomId, function(
+        isRoomExist,
+        roomid,
+        error
+      ) {
+        if (error) {
+          if (error === this.connection.errors.ROOM_NOT_AVAILABLE) {
+            this.$message.error('视频问诊间不存在')
+            return
+          }
+          if (error === this.connection.errors.ROOM_FULL) {
+            this.$message.error('视频问诊间已满')
+            return
+          }
+          this.$message.error(error)
+        }
+      })
     },
     connectVideo() {
       this.connection = new RTCMultiConnection()
@@ -70,39 +83,40 @@ export default {
 }
 </script>
 <style scoped>
-    .container{
-        position: relative;
-        width:  80%;
-        padding: 0;
-        height: 600px;
-        padding-left:10%
-    }
-    .bigVideo{
-        width: 100%;
-        height: 100%;
-        border:1px solid #ccc;
-    }
-    .smallVideo{
-        position: absolute;
-        display: inline-block;
-        right: 0;
-        bottom: 0;
-        width: 200px;
-        height: 150px;
-        cursor: pointer;
-    }
-    .smallVideo:hover{
-        background-color: rgba(255,255,255,0.2);
-        opacity: 0.2;
-    }
-    .video{
-        position:absolute;
-        width: 100%;
-        height: 100%;
-    }
-    .bigVideo p
-    {margin-top:25%;
-        text-align: center;color: #e2e2e2;
-        vertical-align: middle;
-    }
+.container {
+  position: relative;
+  width: 80%;
+  padding: 0;
+  height: 600px;
+  padding-left: 10%;
+}
+.bigVideo {
+  width: 100%;
+  height: 100%;
+  border: 1px solid #ccc;
+}
+.smallVideo {
+  position: absolute;
+  display: inline-block;
+  right: 0;
+  bottom: 0;
+  width: 200px;
+  height: 150px;
+  cursor: pointer;
+}
+.smallVideo:hover {
+  background-color: rgba(255, 255, 255, 0.2);
+  opacity: 0.2;
+}
+.video {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.bigVideo p {
+  margin-top: 25%;
+  text-align: center;
+  color: #e2e2e2;
+  vertical-align: middle;
+}
 </style>
